fb6b1cc193b7b8047be2a78d9429b532f9467605
jonscoke <huangjiapeng159265@gmail.com>
2026-02-25 15:57:29 +0800

    修改两个文件

--- test4.py
+++ test4.py
@@ -1,3 +1,4 @@
 import calendar
 print(calendar.month(2024, 6));
 print("爬爬虾")
+print("爬爬虾吃虾")
--- test5.py
+++ test5.py
@@ -1,3 +1,3 @@
-mylist = ['blue','orange','green']
+mylist = ['blue','orange','green','red','yellow']
 mapped_dictdict = (zip(itr,map(fn,itr)))
 print(mapped_dictdict)
